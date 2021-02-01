b6b9845f0142af813d9352b48944ba183420e44b
Sam Langton <36928709+langtonhugh@users.noreply.github.com>
2021-02-01 09:25:26 +0000

    google mob median only

--- google_mobility.R
+++ google_mobility.R
@@ -49,12 +49,6 @@ outlier_df <- ew_long_df %>%
 ggplot(data = outlier_df) +
   geom_histogram(mapping = aes(x = perc), bins = 60)
 
-# Calculate mean by day.
-ew_meand_df <- ew_long_df %>% 
-  group_by(date, type) %>% 
-  summarise(perc = mean(perc)) %>% 
-  ungroup()
-
 # Plot function.
 plot_fun <- function(x){
   ggplot(data = x) +
@@ -65,25 +59,7 @@ plot_fun <- function(x){
           legend.position = "bottom") 
 }
 
-
-# Plot.
-p1 <- plot_fun(ew_meand_df) +
-  labs(x = NULL, y = "Mean \n change \n from \n baseline \n (%)", colour = NULL) 
-
-ggsave(plot = p1, filename = "visuals/mobility_day_mean_ew.png", height = 14, width = 20, units = "cm")
-
-# Sum by day as per https://datastudio.google.com/reporting/a529e043-e2b9-4e6f-86c6-ec99a5d7b9a4/page/yY2MB?s=ho2bve3abdM.
-ew_sumd_df <- ew_long_df %>% 
-  group_by(date, type) %>% 
-  summarise(perc = sum(perc)) %>% 
-  ungroup()
-
-p2 <- plot_fun(ew_sumd_df) +
-  labs(x = NULL, y = "Sum \n change \n from \n baseline \n (%)", colour = NULL) 
-
-ggsave(plot = p2, filename = "visuals/mobility_day_sum_ew.png", height = 14, width = 20, units = "cm")
-
-# Calculate mean by month.
+# Calculate median by month.
 ew_meanm_df <- ew_long_df %>%
   separate(col = date, into = c("year","month","day"), sep = "-") %>% 
   rename(date = month) %>% 
@@ -92,7 +68,7 @@ ew_meanm_df <- ew_long_df %>%
   ungroup()
 
 # Plot.
-p3 <- plot_fun(ew_meanm_df) +
+p1 <- plot_fun(ew_meanm_df) +
   geom_vline(xintercept = 1, linetype = "dashed") +
   geom_vline(xintercept = 7, linetype = "dashed") +
   annotate(geom = "curve", x = 3.5, xend = 1.1, y = 90, yend = 90, curvature = 0,
@@ -107,38 +83,9 @@ p3 <- plot_fun(ew_meanm_df) +
   theme(axis.title.y = element_text(angle = 0, vjust = 0.5),
         axis.text.x = element_text(hjust = 1.6),
         legend.position = "bottom")
-p3
 
-ggsave(plot = p3, filename = "visuals/mobility_month_median_ew.png", height = 10, width = 16, units = "cm")
-
-# Sum by month.
-ew_summ_df <- ew_long_df %>% 
-  separate(col = date, into = c("year","month","day"), sep = "-") %>% 
-  rename(date = month) %>% 
-  group_by(date, type) %>% 
-  summarise(perc = sum(perc)) %>% 
-  ungroup()
-
-# Plot
-p4 <- plot_fun(ew_summ_df) +
-  scale_x_discrete(labels = c(" ", "Mar", "Apr", "May",
-                              "Jun", "Jul", "Aug", "Sep",
-                              "Oct")) +
-  labs(x = NULL, y = "Sum \n change \n from \n baseline \n (%)", colour = NULL) +
-  theme(axis.title.y = element_text(angle = 0, vjust = 0.5),
-        axis.text.x  = element_text(hjust = 1.75),
-        legend.position = "bottom") +
-  geom_vline(xintercept = 1, linetype = "dashed") +
-  geom_vline(xintercept = 7, linetype = "dashed") +
-  annotate(geom = "curve", x = 3.5, xend = 1.1, y = 750000, yend = 750000, curvature = 0,
-           arrow = arrow(length = unit(1, "mm"))) +
-  annotate(geom = "curve", x = 4.5, xend = 6.9, y = 750000, yend = 750000, curvature = 0,
-           arrow = arrow(length = unit(1, "mm"))) +
-  geom_text(x = 4, y = 750000, label = "Study period", size = 2) +
-  theme(axis.title.y = element_text(angle = 0, vjust = 0.5),
-        legend.position = "bottom")
-
-ggsave(plot = p4, filename = "visuals/mobility_month_sum_ew.png", height = 10, width = 16, units = "cm")
+# Save.
+ggsave(plot = p1, filename = "visuals/mobility_month_median_ew.png", height = 10, width = 16, units = "cm")
 
 
 
